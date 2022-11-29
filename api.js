b654288fd1dc339997ba727a48fa033c62db8dbb
Kel <ite.kelvinhoh@gmail.com>
2022-11-30 00:49:56 +0800

    Added a new API JavaScript file

---
+++ api.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const data = require("./mock_data");
+
+let router = express.Router();
+
+router.get("/users/all", (request, response) => {
+  let users = data.get_all_users();
+  response.send(users);
+});
+
+router.get("/users/by-id", (request, response) => {
+  let user = data.get_user_by_user_id(parseInt(request.query.user_id));
+  response.send(user);
+});
+
+module.exports = { router };
