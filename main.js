e6541a03f204e35a25268b747de54fa0c761242c
Kel <ite.kelvinhoh@gmail.com>
2022-11-30 00:13:50 +0800

    test with main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+const express = require("express");
+//const apis = require ("./api");
+const apis = require("./api");
+const cors = require("cors");
+
+let app = express();
+app.use(cors());
+app.use(express.json());
+app.use(apis.router);
+
+app.listen(3000, (errors) => {
+    if (errors) {
+        console.log(errors);
+    } else {
+        console.log("Server is running on port 3000!");
+    }
+
+})
